1c9f5bc9e978f9c92f71e19c4f7b50d5b7dc7012
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-19 19:47:16 +0530

    Create reverseString function to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let reversedString = "";
+  // loop over a string and get access to characters starting from behind
+  for (let stringIndex = string.length - 1; stringIndex >= 0; stringIndex--) {
+    reversedString = reversedString.concat(string.at(stringIndex));
+  }
+  return reversedString;
 
+  // concat from last character to the first into a new string
 };
 
 // Do not edit below this line
