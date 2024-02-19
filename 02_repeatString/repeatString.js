231ce3b0d09908ef0c2aaae04c91ba8c1c5bbd9a
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-19 19:37:39 +0530

    Complete 2nd exercise and pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    let initialString = "";
+    for (let i = 0; i < num; i++) {
+      initialString += string;
+    }
+    return initialString;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
