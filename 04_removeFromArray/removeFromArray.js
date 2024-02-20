d8fc58ca46d3dca4504917fc4c8366ba6991955b
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-20 07:45:42 +0530

    Create function removeFromArray to remove list of items from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  //loop over the arguments to find the values that are to be removed
 
+  let itemsToRemove = [];
+  for (let i = 1; i < arguments.length; i++) {
+    itemsToRemove.push(arguments[i]);
+  }
+
+  // remove all instances of items from the array
+  function checkItemToRemove(arrayItem) {
+    for (itemToRemove of itemsToRemove) {
+      if (arrayItem === itemToRemove) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // loop over
+  return array.filter(checkItemToRemove);
+  // get the final array and return it
 };
 
 // Do not edit below this line
