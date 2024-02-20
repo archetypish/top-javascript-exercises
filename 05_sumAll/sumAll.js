871ec74ae3fde5e264fbd21356cc73f504e87a0f
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-20 08:09:57 +0530

    Create sumAll function to sum a linear ap:

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (startNumber, endNumber) {
+  // check if these are positive numbers
+  if (
+    typeof startNumber === "number" &&
+    typeof endNumber === "number" &&
+    startNumber >= 0 &&
+    endNumber >= 0
+  ) {
+    let sum = 0;
+    const smallerNumber = Math.min(startNumber, endNumber);
+    const largerNumber = Math.max(startNumber, endNumber);
 
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
