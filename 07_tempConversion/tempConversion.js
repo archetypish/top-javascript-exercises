b048135a83899ab2241333dde9e6610acb49f9e3
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-20 12:21:17 +0530

    Create temp conversion functions for celsius and fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahrenheit) {
+  const tempInCelsiusPrecise = ((tempInFahrenheit - 32) * 5) / 9;
+  const tempInCelsiusRounded = Math.round(tempInCelsiusPrecise * 10) / 10;
+  return tempInCelsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelsius) {
+  const tempInFahrenheitPrecise = (tempInCelsius * 9) / 5 + 32;
+  const tempInFahrenheitRounded = Math.round(tempInFahrenheitPrecise * 10) / 10;
+  return tempInFahrenheitRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
