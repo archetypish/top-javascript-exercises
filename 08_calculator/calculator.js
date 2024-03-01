19f002380956bbc7ba4fe7378ae94cf3c579c511
Arun Chaudhary <theindiancoder44@gmail.com>
2024-03-01 08:11:22 +0530

    Create calculator funcs as per the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, item) => {
+    sum += item;
+    return sum;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((agg, item) => {
+    agg *= item;
+    return agg;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let result = 1;
+  for (let i = 0; i < b; i++) {
+    result = result * a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  } else if (n < 0) {
+    return "invalid";
+  }
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
