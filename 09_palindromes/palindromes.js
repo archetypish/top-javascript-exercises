d838c6cd12b555a04cdc7326547f94d6c716a2fb
Arun Chaudhary <theindiancoder44@gmail.com>
2024-03-04 06:25:29 +0530

    Create function to test palindrome sequence

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // get original string
+  // str = "race car!";
+  // remove all the spaces and punctuations and lowercase it
+  let filters = [" ", ",", ".", "?", "!"];
+  let cleanStr = "";
+  for (char of str) {
+    if (filters.includes(char)) continue;
+    cleanStr += char.toLowerCase();
+  }
+  // store this new updated string
 
+  // reverse this updated string
+  let reversedStr = "";
+  for (let i = cleanStr.length - 1; i >= 0; i--) {
+    reversedStr = reversedStr.concat(cleanStr.at(i));
+  }
+
+  return reversedStr === cleanStr;
 };
 
 // Do not edit below this line
