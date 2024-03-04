e7ab738ba1e81b4f6ed70abb160571a30e6aadb0
Arun Chaudhary <theindiancoder44@gmail.com>
2024-03-04 07:15:58 +0530

    Create function to book titles from books object

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  // from an array of objects of books, get the titles only into an array
 
+  // array of objects with titles and author
+  // array of titles
+
+  return books.map((book) => book.title);
 };
 
 // Do not edit below this line
