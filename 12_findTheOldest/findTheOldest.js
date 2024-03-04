35f07bb4ef4725c102866242b4fa52e5b5ab5267
Arun Chaudhary <theindiancoder44@gmail.com>
2024-03-04 07:54:24 +0530

    Create a function to get the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // people array with person objects with name, birth and death years
+  // name of the oldest person
+  // algo
+  // sort people from oldest to youngest
+  // return the first entry
 
+  // handle 2 cases where year of death is missing
+  // if the year of birth is too new (non oldest person)
+  // if the year of birth is too old (oldest person)
+
+  return people
+    .map((person) => {
+      if (!("yearOfDeath" in person)) {
+        let yearCurrent = new Date();
+        yearCurrent = yearCurrent.getFullYear();
+        person.age = yearCurrent - person.yearOfBirth;
+      } else {
+        person.age = person.yearOfDeath - person.yearOfBirth;
+      }
+
+      return person;
+    })
+    .sort((a, b) => (a.age > b.age ? -1 : 1))[0];
 };
 
 // Do not edit below this line
